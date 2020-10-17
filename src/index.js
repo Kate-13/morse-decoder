ba821d06206ae9b20b913922b3fd1c7c8dae79bd
Kate-13 <kate_boyko@mail.ru>
2020-10-17 20:04:55 +0300

    feat: implement decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,20 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let letter = '';
+    let res = expr.replace(/([*\d]{10})/g, '$1 ').trim().split(' '); //separate string on parts by 10 symbols and split them
+
+    for (let i=0; i < res.length; i++){
+        let code = res[i].replace(/^0+/, '').replace(/(\d{2})/g, '$1 ').replace(/10 /g, '.').replace(/11 /g, '-'); //delete lead 000, separate on parts by 2, 10 replace by '.', 11 -> '-'
+        if (code === '**********') {
+        letter = ' ';
+        } else {
+        letter = MORSE_TABLE[code];
+        }
+        result= result + letter;
+    }
+    return result;
 }
 
 module.exports = {
